e2d603011b09b770817952a9582c41c252d73591
faizunnur <faizunnur71@gmail.com>
2026-03-26 21:32:49 +0600

    chore: bootstrap Spring Boot starter from Spring Initializr

---
+++ src/test/java/com/example/tst13/Tst13ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tst13;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tst13ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
