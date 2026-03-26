5396c17654e89a194204d2162f1b703526e4c438
faizunnur <faizunnur71@gmail.com>
2026-03-26 21:32:53 +0600

    chore: bootstrap Spring Boot starter from Spring Initializr

---
+++ src/main/java/com/example/tst13/Tst13Application.java
@@ -0,0 +1,13 @@
+package com.example.tst13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tst13Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tst13Application.class, args);
+	}
+
+}
